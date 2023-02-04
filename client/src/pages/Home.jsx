cbc0a6cc8986c623151a1333b8374c8fb802f77b
arik hozias <ahozias@gmail.com>
2023-02-04 22:54:53 +0200

    start createPost

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Spin } from 'antd';
 import { Loader, Card, FormField } from '../components';
+
 function Home() {
-	const [loading, isLoading] = useState(true);
+	const [loading, isLoading] = useState(false);
 	const [allPosts, setAllPosts] = useState([null]);
+	const [searchText, setSearchText] = useState('asasa');
+
+	const RenderCards = ({ data, title }) => {
+		if (data?.length > 0) {
+			return data.map((post) => <Card key={post._id} {...post} />);
+		}
+		return <h2 className='mt-5 font-bold text-[#6449ff] uppercase'>{title}</h2>;
+	};
+
 	return (
 		<section className='max-w-7xl mx-auto'>
 			<div>
@@ -24,7 +34,21 @@ function Home() {
 						<Spin />
 					</div>
 				) : (
-					<></>
+					<>
+						{searchText && (
+							<h2 className='font-medium text-[#666e75] text-xl mb-3'>
+								Showing results for{' '}
+								<span className='text-[#222328]'>{searchText}</span>
+							</h2>
+						)}
+						<div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+							{searchText ? (
+								<RenderCards data={[]} title='No results Found' />
+							) : (
+								<RenderCards data={[]} title='No Post Found' />
+							)}
+						</div>
+					</>
 				)}
 			</div>
 		</section>
