c54bd386e21fe2eaef56c4e4d32588129cbb8fcf
arik hozias <ahozias@gmail.com>
2023-02-05 23:04:43 +0200

    til input field

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-function FormField() {
-	return <div>FormField</div>;
+function FormField({
+	labelName,
+	type,
+	name,
+	placeHolder,
+	value,
+	handleChange,
+	isSurpriseMe,
+	handleSurpriseMe,
+}) {
+	return (
+		<div>
+			<div className='flex items-center gap-2 mb-2'>
+				<label
+					htmlFor={name}
+					className='block text-sm font-medium text-gray-900'>
+					{labelName}
+				</label>
+				{isSurpriseMe && (
+					<button
+						className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+						type='button'
+						onClick={handleSurpriseMe}>
+						Surprise Me
+					</button>
+				)}
+			</div>
+			<input
+				type={type}
+				id={name}
+				name={name}
+				placeholder={placeHolder}
+				value={value}
+				required
+			/>
+		</div>
+	);
 }
 
 export default FormField;
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Spin } from 'antd';
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField } from '../components';
 
 function CreatePost() {
-	return <div>CreatePost</div>;
+	const navigate = useNavigate();
+	const [form, setForm] = useState({ name: '', prompt: '', photo: '' });
+	const [generatingImg, setGeneratingImg] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+	const handleSubmit = () => {};
+	const handleChange = (e) => {};
+	const handleSurpriseMe = () => {};
+
+	return (
+		<section className='max-w-71 mx-auto'>
+			<div>
+				<h1 className='font-extrabold text-[32px]'>Create</h1>
+				<p className='mt-2 text-[#666e74] text-[16px] max-w[500px]'>
+					Create imagiative and visually stunning images through DALL-E AI
+				</p>
+			</div>
+			<form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+				<div className='flex flex-col gap-5'>
+					<FormField
+						labelName='Your Name'
+						type='text'
+						name='name'
+						placeHolder='Arik hozi'
+						value={form.name}
+						handleChange={handleChange}
+					/>
+					<FormField
+						labelName='prompt'
+						type='text'
+						name='prompt'
+						placeHolder='A plush toy robot sitting against a yellow wall'
+						value={form.name}
+						handleChange={handleChange}
+						isSurpriseMe
+						handleSurpriseMe={handleSurpriseMe}
+					/>
+				</div>
+			</form>
+		</section>
+	);
 }
 
 export default CreatePost;
