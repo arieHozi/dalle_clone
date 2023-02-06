8b61275272f1071dde7eda0c6d9bf50fd662f9ba
arik hozias <ahozias@gmail.com>
2023-02-06 22:22:27 +0200

    init server

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -34,6 +34,7 @@ function FormField({
 				placeholder={placeHolder}
 				value={value}
 				required
+				className='bg-gray-80 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none black w-full p-3'
 			/>
 		</div>
 	);
